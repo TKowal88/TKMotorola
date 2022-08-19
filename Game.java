7b05d2c5626a7214cc6289e6315154fb662b522d
Tomek Kowalczyk <tomekkowalczyk@tomeks-macbook-air.home>
2022-08-19 14:10:47 +0200

    Added logic for choosing a level

--- Game.java
+++ Game.java
@@ -21,9 +21,26 @@ public class Game {
             }   
         }
     }
+    public static class Level {
+        int difficulty = 0;
+        public Level () {
+        Scanner digit = new Scanner(System.in);
+        System.out.println("Choose your difficulty level\n Press 1 for EASY\n Press 2 for HARD");
+        try {
+            difficulty = digit.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("\nInput numbers only");
+        }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the game!");
-        Load data = new Load();   
-        System.out.println(data.wordArray[0]);
-}
-}
+        Load data = new Load(); 
+        Level level = new Level();
+        while (level.difficulty < 1 || level.difficulty > 2) {
+            System.out.println("\nPlease enter 1 or 2 only\n");
+            level = new Level();
+        }  
+        System.out.println(level.difficulty);  
+        }
+    }
