1455f6b51c224482c937b1ae6e311775080f1359
Tomek Kowalczyk <tomekkowalczyk@tomeks-macbook-air.home>
2022-08-19 17:21:00 +0200

    added print method to display word arrays

--- Game.java
+++ Game.java
@@ -9,18 +9,21 @@ public class Game {
     public static class Load {
         String[] wordArray = new String[100];
         public Load() {
-            try {  
+            try {
             File words = new File("Words.txt");
             Scanner input = new Scanner(words);
             int i = 0;
+            
             while (input.hasNextLine()) {
                 wordArray[i] = input.nextLine();
                 i++;
             }
+            
             input.close();
             } catch (FileNotFoundException e) {
                 System.out.println("Error");
             }   
+            
         }
     }
     public static class Level {
@@ -39,9 +42,14 @@ public class Game {
     public static class Game1 {
         String[] rowA = new String[4];
         String[] rowB = new String[4];
+        int [] columns = {1, 2, 3, 4};
+        String level = "easy";
+        int chances = 10;
         public Game1(String[] array){
             ArrayList<Integer> list100 = new ArrayList<Integer>();
             ArrayList<Integer> list4 = new ArrayList<Integer>();
+            
+
             for (int i = 0; i < 100; i++) {
                 list100.add(i);
             }
@@ -54,14 +62,33 @@ public class Game {
 
             for (int i = 0; i < 4; i++) {
                 rowA[i] = array[list100.get(i)];
-                System.out.println(rowA[i]);
             }
             for (int i = 0; i < 4; i++) {
-                rowB[i] = rowA[list4.get(i)];
-                System.out.println(rowB[i]);
+                rowB[i] = rowA[list4.get(i)];                
+            }           
             }
-            
-            
+            public void print() {
+                
+                System.out.println("Level: " + level);
+                System.out.println("Guess chances: " + chances);
+                System.out.print("  ");
+                for (int i = 0; i < 4; i++){
+                    int length = rowA[i].length() + 1;
+                    System.out.printf("%" + -length + "d", columns[i]);                   
+                }
+                System.out.println();
+                System.out.print("A ");
+                for (String word : rowA) {
+                    System.out.print(word + " ");                    
+                }
+                System.out.println();
+                System.out.print("B ");
+                for (String word : rowB) {
+                    System.out.print(word + " ");                    
+                }
+                System.out.println();
+
+
             }
         }
     
@@ -77,6 +104,7 @@ public class Game {
         
         if (level.difficulty == 1) {
             Game1 game = new Game1(data.wordArray);
+            game.print();
         }
         else {
 
