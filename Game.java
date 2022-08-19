c006ad1ed68dac067f6f8bfbe59aace53a63f1b8
Tomek Kowalczyk <tomekkowalczyk@tomeks-macbook-air.home>
2022-08-19 15:16:48 +0200

    Added Game1 class with logic for randomizing word arrays

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Game {
     public static class Load {
@@ -32,7 +34,38 @@ public class Game {
             System.out.println("\nInput numbers only");
         }
         }
+ 
     }
+    public static class Game1 {
+        String[] rowA = new String[4];
+        String[] rowB = new String[4];
+        public Game1(String[] array){
+            ArrayList<Integer> list100 = new ArrayList<Integer>();
+            ArrayList<Integer> list4 = new ArrayList<Integer>();
+            for (int i = 0; i < 100; i++) {
+                list100.add(i);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                list4.add(i);
+            }
+            Collections.shuffle(list100);
+            Collections.shuffle(list4);
+
+            for (int i = 0; i < 4; i++) {
+                rowA[i] = array[list100.get(i)];
+                System.out.println(rowA[i]);
+            }
+            for (int i = 0; i < 4; i++) {
+                rowB[i] = rowA[list4.get(i)];
+                System.out.println(rowB[i]);
+            }
+            
+            
+            }
+        }
+    
+    
     public static void main(String[] args) {
         System.out.println("Welcome to the game!");
         Load data = new Load(); 
@@ -40,7 +73,13 @@ public class Game {
         while (level.difficulty < 1 || level.difficulty > 2) {
             System.out.println("\nPlease enter 1 or 2 only\n");
             level = new Level();
-        }  
-        System.out.println(level.difficulty);  
+        }   
+        
+        if (level.difficulty == 1) {
+            Game1 game = new Game1(data.wordArray);
         }
-    }
+        else {
+
+        }
+        }
+}
